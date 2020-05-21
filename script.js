c59371f47da2132ceb91df63f5d6c1d6a29de7d0
Richard <rkj128@gmail.com>
2020-05-20 20:29:02 -0700

    forecast added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let units = 'imperial';
 let searchMethod;
 
 function getSearchMethod(searchTerm) {
-    if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+    if (searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
         searchMethod = 'zip';
     else
         searchMethod = 'q';
@@ -11,7 +11,7 @@ function getSearchMethod(searchTerm) {
 
 function searchWeather(searchTerm) {
     getSearchMethod(searchTerm);
-    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
         return result.json();
     }).then(result => {
         init(result);
@@ -19,12 +19,12 @@ function searchWeather(searchTerm) {
     })
 }
 
-function init(resultFromServer){
+function init(resultFromServer) {
     switch (resultFromServer.weather[0].main) {
         case 'Clear':
             document.body.style.backgroundImage = 'url("clear.jpg")';
             break;
-        
+
         case 'Clouds':
             document.body.style.backgroundImage = 'url("cloudy.jpg")';
             break;
@@ -32,13 +32,13 @@ function init(resultFromServer){
         case 'Rain':
         case 'Drizzle':
         case 'Mist':
-            document.body.style.backgroundImage = 'url("rain.jpg")';    
+            document.body.style.backgroundImage = 'url("rain.jpg")';
             break;
 
         case 'Thunderstorm':
             document.body.style.backgroundImage = 'url("storm.jpg")';
             break;
-            
+
         case 'Snow':
             document.body.style.backgroundImage = 'url("snow.jpg")';
             break;
@@ -72,37 +72,37 @@ function setPositionForWeatherInfo() {
     let weatherContainerHeight = weatherContainer.clientHeight;
     let weatherContainerWidth = weatherContainer.clientWidth;
 
-    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
-    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3}px)`;
+    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth / 2}px)`;
+    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight / 1.3}px)`;
     weatherContainer.style.visibility = 'visible';
 }
 
 document.getElementById('searchBtn').addEventListener('click', () => {
     let searchTerm = document.getElementById('searchInput').value;
-    if(searchTerm){
+    if (searchTerm) {
         searchWeather(searchTerm);
     }
     displayCity(searchTerm);
-    document.getElementById('searchInput').value='';
+    document.getElementById('searchInput').value = '';
     searchforecast(searchTerm);
 })
 
-$("body").on("click",".city",function(){
+$("body").on("click", ".city", function () {
     var city = $(this).attr("value");
     searchWeather(city);
 })
 
-function displayCity (cityName){
+function displayCity(cityName) {
     var city = $(this).attr("city-name");
     var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid={53d2378036e10278637205320c39dd84}";
-    var cityBtn =$("<button class='city'>");
-        cityBtn.text(cityName)
-        cityBtn.attr("value", cityName)
-        $("#city").prepend(cityBtn);
+    var cityBtn = $("<button class='city'>");
+    cityBtn.text(cityName)
+    cityBtn.attr("value", cityName)
+    $("#city").prepend(cityBtn);
 }
 
-$("body").on("click",".forecast",function(){
-    var  forecast = $(this).attr("value");
+$("body").on("click", ".forecast", function () {
+    var forecast = $(this).attr("value");
     searchWeather(forecastEl);
 })
 
@@ -112,46 +112,49 @@ function searchforecast(searchTerm) {
 
 
     getSearchMethod(searchTerm);
-    fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&appid=53d2378036e10278637205320c39dd84").then(result => {
-    return result.json();
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&appid=53d2378036e10278637205320c39dd84").then(result => {
+        return result.json();
     })
-    .then(result => {
-        console.log(result);
+        .then(result => {
+            console.log(result);
 
-        $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast: </h4>").append("<div class=\"row\">");
+            $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast: </h4>").append("<div class=\"row\">");
 
-        for(var i = 0; i < 5; i++){
-            result.list[i]
-            console.log(result.list[i]);
-            
-            const colEL = $("<div>").addClass("col-md-2");
+            for (var i = 0; i < 5; i++) {
+                result.list[i]
+                console.log(result.list[i]);
 
-            const cardEL = $("<card>").addClass("card bg-primary text-white");
+                const colEL = $("<div>").addClass("col-md-2");
 
-            const cardBodyEL = $("<div>").addClass("card-body p-2");
+                const cardEL = $("<card>").addClass("card bg-primary text-white");
 
-            //extract data from current element
-            const titleEl = $("<h5>").addClass("card-title").text(new Date(response.list[i].dt_txt).toLocalDateSting());
-            
-            const tempEL = $("<p>").addClass("card-text").text(`Temp: ${response.list[i].main.temp_max}`);
-            const humidityEL = $("<p>").addClass("card-text").text(`humidity: ${response.list[i].main.humidity}`);
+                const cardBodyEL = $("<div>").addClass("card-body p-2");
 
-            //append all data together in cardEL
-            cardBodyEL.append(titleEl, tempEL, humidityEL);
-            cardlEL.append(cardBodyEL);
+                //extract data from current element
+                const titleEl = $("<h5>").addClass("card-title").text(new Date(result.list[i].dt_txt).toString());
 
-            colEL.append(cardEL);
+                const tempEL = $("<p>").addClass("card-text").text(`Temp: ${result.list[i].main.temp_max}`);
+                const humidityEL = $("<p>").addClass("card-text").text(`humidity: ${result.list[i].main.humidity}`);
 
-            $("forecast .row").append(colEL);
-        
+                //append all data together in cardEL
+                cardBodyEL.append(titleEl, tempEL, humidityEL);
+                cardEL.append(cardBodyEL);
 
-        }
-        
-    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
-    let tempuratureElement = document.getElementById('tempurature');
-    let humidityElement = document.getElementById('humidity');
-    let windSpeedElement = document.getElementById('windSpeed');
-    let cityHeader = document.getElementById('cityHeader');
-    let weatherIcon = document.getElementById('documentIconImg');
-    })
+                colEL.append(cardEL);
+                // (SPACE) looking at forcast and latching on to child of "row".
+                $("#forecast .row").append(colEL);
+
+
+            }
+
+            let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+            let tempuratureElement = document.getElementById('tempurature');
+            let humidityElement = document.getElementById('humidity');
+            let windSpeedElement = document.getElementById('windSpeed');
+            let cityHeader = document.getElementById('cityHeader');
+            let weatherIcon = document.getElementById('documentIconImg');
+        })
+
+
 }
+
